7e1509c84deb5cac2ed275bd43a6d7f2f7cf6faf
Cameron Olson <cmsolson75@gmail.com>
2025-06-02 21:47:20 +0000

    Organization of dir

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from lightning.pytorch.loggers import WandbLogger
+
 from hydra.utils import instantiate, get_class
 import lightning as L
 import torch
 
-@hydra.main(config_path="src/configs", config_name="config", version_base="1.3")
+@hydra.main(config_path="configs", config_name="config", version_base="1.3")
 def main(cfg: DictConfig):
     block_cls = get_class(cfg.model.block._target_)
     stem = instantiate(cfg.model.stem)
@@ -25,7 +27,6 @@ def main(cfg: DictConfig):
     # Callbacks
     callbacks = [instantiate(cb) for cb in cfg.trainer.callbacks.values()]
     logger = instantiate(cfg.trainer.logger)
-
     # Trainer
     trainer = L.Trainer(
         logger=logger,
@@ -37,8 +38,13 @@ def main(cfg: DictConfig):
         precision=cfg.trainer.precision,
         benchmark=True,
     )
+    ckpt_path = cfg.checkpoint.resume.ckpt_path
+    if ckpt_path:
+        print(f"Resuming from checkpoint: {cfg.checkpoint.resume.ckpt_path}")
+    else:
+        print("Starting training from scratch")
 
-    trainer.fit(lightning_module, datamodule=datamodule)
+    trainer.fit(lightning_module, datamodule=datamodule, ckpt_path=ckpt_path)
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
